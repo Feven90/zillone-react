53621c9d90e73c7b70b85fe824f62f50a0453489
feven mulugeta <fevenmulugeta@fevens-MacBook-Pro.local>
2018-12-11 20:14:23 -0600

    setup authentication

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import authRequests from '../../helpers/data/authRequests';
 import './Auth.scss';
 
 class Auth extends React.Component {
+  authenticateUser = (e) => {
+    e.preventDefault(); // telling it to do only specific thing not the default of click event
+    authRequests.authenticate().then(() => {
+
+    }).catch(err => console.error('there was an error with auth', err));
+  }
+
   render() {
     return (
-            <div className="Auth">
-                <button className="btn btn-danger">Login</button>
-                </div>
+        <div className="Auth">
+            <button className="btn btn-danger" onClick={this.authenticateUser}>Login</button>
+        </div>
     );
   }
 }
