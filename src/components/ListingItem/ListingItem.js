5aebd262e487bcb1a17022148aaf12e6acb28829
feven mulugeta <fevenmulugeta@fevens-MacBook-Pro.local>
2018-12-15 11:20:39 -0600

    listing items styled and printing to DOM

--- src/components/ListingItem/ListingItem.js
+++ src/components/ListingItem/ListingItem.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './ListingItem.scss';
+import listingShape from '../../helpers/propz/lsitingShape';
 
 class ListingItem extends React.Component {
+  static propTypes = {
+    listing: listingShape,
+  }
+
   render() {
     const { listing } = this.props;
     return (
-      <h2>{ listing.address}</h2>
+      <li className="listing-item text-center">
+        <span className="col-7">{listing.address}</span>
+        <span className="col-3">{listing.price}</span>
+      </li>
     );
   }
 }
