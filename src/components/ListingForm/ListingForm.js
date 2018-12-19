33a8c68c1757f6b71b59efbafd1c911396c7c404
feven mulugeta <fevenmulugeta@fevens-MacBook-Pro.local>
2018-12-18 19:18:40 -0600

    in-progress

--- src/components/ListingForm/ListingForm.js
+++ src/components/ListingForm/ListingForm.js
@@ -5,8 +5,21 @@ class ListingForm extends React.Component {
   render() {
     return (
       <div className="listing-form col">
-        <h2>Listing Form</h2>
-      </div>
+        <h2>Add New Listing:</h2>
+      <form>
+      <div class="form-group">
+        <label for="address">Address:</label>
+          <input
+            type="text"
+            class="form-control"
+            id="address"
+            aria-describedby="addressHelp"
+            placeholder="234 Edge Moor Dr, 21783"
+            />
+        </div>
+        <button className="btn btn-danger">Save Listing</button>
+      </form>
+  </div>
     );
   }
 }
